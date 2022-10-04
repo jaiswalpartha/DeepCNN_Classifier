502d753d54c8a60afc82da1436a61b593de2a82b
jaiswalpartha <parthajaiswal111@gmail>
2022-10-04 19:02:18 +0530

    adding common utillty func

--- src/deepClassifier/utils/common.py
+++ src/deepClassifier/utils/common.py
@@ -1,5 +1,4 @@
 import os
-from tkinter import E
 from typing import Any
 from box.exceptions import BoxValueError
 import yaml
@@ -8,12 +7,13 @@ import json
 import joblib
 from ensure import ensure_annotations
 from box import ConfigBox
-from pathlib import Path, path 
-from type import any
+from pathlib import Path 
+from typing import Any
+
 
 
 @ensure_annotations
-def read_yaml(path_to_yaml:path)->ConfigBox:
+def read_yaml(path_to_yaml:Path)->ConfigBox:
     '''reads yaml file and returns
     Args:
         path_to_yaml(str): path like input
